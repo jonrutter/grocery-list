a87726a07a1dfbd43945638f9e569ae75a185d70
Jon Rutter <contact@jonrutter.io>
2022-12-01 14:43:01 -0500

    Add button component

---
+++ src/components/button.tsx
@@ -0,0 +1,11 @@
+export const Button: React.FC<React.ComponentPropsWithoutRef<'button'>> = ({
+  children,
+  ...rest
+}) => (
+  <button
+    {...rest}
+    className="bg-emerald-700 hover:bg-emerald-600 py-2 px-4 text-white rounded-md leading-none transition-all"
+  >
+    {children}
+  </button>
+);
