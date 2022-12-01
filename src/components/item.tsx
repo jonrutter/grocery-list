c0dee7dae23dcadc39162ae8f2ca07ac6aff54c8
Jon Rutter <contact@jonrutter.io>
2022-12-01 14:47:25 -0500

    Add list item component

---
+++ src/components/item.tsx
@@ -0,0 +1,57 @@
+import React from 'react';
+import { BsCheck } from 'react-icons/bs';
+
+// types
+import type { ListItem } from '../app/app';
+
+type Props = {
+  item: ListItem;
+  onCheck: (_: string) => void;
+};
+
+export const Item: React.FC<Props> = ({ item, onCheck }) => {
+  const { label, done, id } = item;
+
+  const handleCheck = (e: React.ChangeEvent<HTMLInputElement>) => {
+    onCheck(id);
+  };
+
+  return (
+    <li className="py-1 border-b border-b-gray-200 mb-2 block w-full overflow-hidden">
+      <label className="relative user-select-none flex items-center w-full overflow-hidden cursor-pointer">
+        <span className="sr-only">Check off {label} from list</span>
+        {/* custom checkbox */}
+        <div className="block relative select-none w-4 h-4">
+          <input
+            type="checkbox"
+            checked={done}
+            onChange={handleCheck}
+            className="absolute opacity-0 h-0 w-0"
+          />
+          {/* box */}
+          <span
+            className={`absolute inset-0 border rounded-md transition-all ${
+              done
+                ? 'bg-emerald-50 border-emerald-400'
+                : 'bg-transparent border-gray-400'
+            }`}
+          ></span>
+          {/* check */}
+          <BsCheck
+            aria-hidden
+            className={`absolute inset-0 text-emerald-700 transition-all ${
+              done ? 'opacity-100' : 'opacity-0'
+            }`}
+          />
+        </div>
+        <span
+          className={`ml-2 flex-1 inline-block ${
+            done ? 'line-through text-gray-500' : ''
+          }`}
+        >
+          {label}
+        </span>
+      </label>
+    </li>
+  );
+};
